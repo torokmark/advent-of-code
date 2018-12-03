ae3a0286fcfe38afb807eb6d028039fbd86cabb2
Torok Mark <torokmark@gmx.com>
2018-12-03 11:17:13 +0100

    Add comments

--- 2018/02/part_2.rb
+++ 2018/02/part_2.rb
@@ -1,8 +1,5 @@
 ids = File.readlines('input').map { |line| line.strip }
 
-a = ids[1].bytes
-b = ids[4].bytes
-
 class String
   def xor_with(other_string)
     self.bytes.zip(other_string.bytes).map { |a, b|  a ^ b}.pack('c*')
@@ -10,16 +7,16 @@ class String
 end
 
 ids.each do |id|
-  arr = ids.map { |l| 
-    l.xor_with(id)
-      .bytes
-      .each_with_object(Hash.new(0)) { |word, counts| 
-        counts[word] += 1 
+  arr = ids.map { |l|  # map values to xored 
+    l.xor_with(id)     # xor the two strings
+      .bytes           # convert the xored results to bytes
+      .each_with_object(Hash.new(0)) { |word, counts|   
+        counts[word] += 1     # counts the frequency of each
       } 
   }.select { |h| 
-    h.values.count == 2 
+    h.values.count == 2  # select just those which contains only two different pairs
   }.select { |h|
-    h.has_value?(1)
+    h.has_value?(1)   # select those where one of them is one (one distance far, the other one is '0' => something)
   }
   p "#{ id } #{ arr }" if not arr.empty?
 end

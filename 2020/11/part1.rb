1008406d12f69c827b2f8be5ad9e6fbdb0bc8f1d
Torok Mark <torok.marko@gmail.com>
2020-12-13 09:57:56 +0100

    Add eleventh part1

--- 2020/11/part1.rb
+++ 2020/11/part1.rb
@@ -1,40 +1,60 @@
 
-seats = File.readlines('sample').map { |line| line.strip }
+seats = File.readlines('input').map { |line| line.strip }
+
+def pr(new_seats)
+  new_seats.each do |line|
+    p line
+  end
+end
 
 
 def any_occupied?(seats, row, col)
-  (-1..1).each do |r|
-    (-1..1).each do |c|
-      if (0..seats.size-1).include?(row + r) && (0..seats.first.size-1).include?(col + c)
-        if '#' == seats[row + r][col + c]
-          return true 
-        end
-      end
+  idxs = [-1, 0, 1].product([-1, 0, 1])
+  idxs.delete([0, 0])
+  idxs.each do |pair|
+    if (0..seats.size-1).include?(row + pair.first) && (0..seats.first.size-1).include?(col + pair.last)
+      return true if '#' == seats[row + pair.first][col + pair.last]
     end
   end
   false
 end
 
-def num_of_empty?(seats, row, col)
+def num_of_occupied(seats, row, col)
   num_of_empty = 0
-  (-1..1).each do |r|
-    (-1..1).each do |c|
-      if (0..seats.size-1).include?(row + r) && (0..seats.first.size-1).include?(col + c)
-        num_of_empty += 1 if 'L' == seats[row + r][col + c]
-      end
+  idxs = [-1, 0, 1].product([-1, 0, 1])
+  idxs.delete([0, 0])
+  idxs.each do |pair|
+    if (0..seats.size-1).include?(row + pair.first) && (0..seats.first.size-1).include?(col + pair.last)
+      num_of_empty += 1 if '#' == seats[row + pair.first][col + pair.last] 
     end
   end
   num_of_empty
+end
+
+def all_occupied(seats)
+  occupied = 0
+  seats.each do |row|
+    occupied += row.chars.count { |col| col == '#' }
+  end
+  occupied
 end
 
 def change(seats)
   new_seats = seats.map(&:clone)
   0.upto(seats.size - 1) do |row|
     0.upto(seats.first.size - 1) do |col|
-      if 'L' == seats[row][col] && !any_occupied?(seats, row, col)
-        new_seats[row][col] = '#' 
-      elsif '#' == seats[row][col] && 4 <= num_of_empty?(seats, row, col)
-        new_seats[row][col] = 'L'
+      if 'L' == seats[row][col] 
+        if !any_occupied?(seats, row, col)
+          new_seats[row][col] = '#' 
+        else
+          new_seats[row][col] = 'L'
+        end
+      elsif '#' == seats[row][col] 
+        if 4 <= num_of_occupied(seats, row, col)
+          new_seats[row][col] = 'L'
+        else
+          new_seats[row][col] = '#'
+        end
       elsif '.' == seats[row][col]
         new_seats[row][col] = '.'
       end
@@ -43,33 +63,15 @@ def change(seats)
   new_seats
 end
 
-#p occupied?(seats, 2, 3)
-#p four_and_more_empty?(seats, 1, 1)
-
-=begin
-seats.each do |line|
-  p line
-end
-p '==================='
-new_seats = change(seats)
-new_seats.each do |line|
-  p line
-end
-p "===================="
-seats.each do |line|
-  p line
-end
-=end
 new_seats = seats.map(&:clone)
 
-loop do 
+new_seats = change(new_seats)
+
+loop do
   seats = new_seats
   new_seats = change(seats)
-  p '1'
-  break if seats.eql? new_seats
+  break if new_seats == seats
 end
 
-new_seats.each do |line|
-  p line
-end
-
+pr new_seats
+p all_occupied(new_seats)

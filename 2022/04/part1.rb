ecf4903bede2fddc8b8a64d4a15a3d0612732a18
Torok Mark <torok.marko@gmail.com>
2022-12-19 01:44:03 +0100

    Add 2022, day 4

---
+++ 2022/04/part1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/exec ruby
+
+def solve(lines)
+  lines
+    .map { |line| line.split(",") }
+    .map { |line| [line.first.split("-").map(&:to_i), line.last.split("-").map(&:to_i)] }
+    .map { |line| [Range.new(line.first[0], line.first[1]), Range.new(line.last[0], line.last[1])] }
+    .map { |line| line.first.cover?(line.last) || line.last.cover?(line.first) }
+    .select { |e| e }
+    .count
+end
+
+file = File.open("input.txt")
+lines = file.read.split("\n")
+
+p solve(lines)
